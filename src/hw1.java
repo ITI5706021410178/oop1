602227dc29e6695cd01cd2c6995ca078f29cbfd1
ITI5706021410178 <5706021410178@fitm.kmutnb.ac.th>
2016-02-18 21:42:46 +0700

    1

--- src/hw1.java
+++ src/hw1.java
@@ -1,9 +1,18 @@
-
+import java.io.*;
+import javax.swing.JOptionPane;
+import java.util.*;
 public class hw1 {
 
+	int Sec,Min,Hour,Day;
+	
+	public void getData()
+	{
+		Sec = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter Sec","Input",JOptionPane.QUESTION_MESSAGE));
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+
 
 }
