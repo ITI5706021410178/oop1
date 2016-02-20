eed98da65f71997e91136f4ff2524fcf3e263252
ITI5706021410178 <5706021410178@fitm.kmutnb.ac.th>
2016-02-20 22:21:53 +0700

    3

--- src/hw1.java
+++ src/hw1.java
@@ -9,6 +9,17 @@ public class hw1 {
 	{
 		Sec = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter Sec","Input",JOptionPane.QUESTION_MESSAGE));
 	}
+	public void Calc()
+	{
+		Min = Sec / 60;
+		Sec = Sec % 60;
+		
+		Hour = Min / 60;
+		Min = Min % 60;
+		
+		Day = Hour/24;
+		Hour = Hour % 24;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
