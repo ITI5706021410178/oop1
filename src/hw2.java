9dcb0d1364dcedcbf71ea5f47d76d16e9998ae0c
ITI5706021410178 <5706021410178@fitm.kmutnb.ac.th>
2016-02-21 18:23:03 +0700

    4

--- src/hw2.java
+++ src/hw2.java
@@ -3,4 +3,10 @@ import javax.swing.JOptionPane;
 import java.util.*;
 public class hw2 {
       int A,E,I,O,U;
+      	public String getData()
+	{
+		String name = JOptionPane.showInputDialog(null,"Enter name ","InputData",JOptionPane.QUESTION_MESSAGE);
+		name = name.toUpperCase();
+		return name;
+	}
 }
